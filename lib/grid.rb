49a891f8a06a8a7e47155fdf1c113e2fd1cc8013
James Hunter <james.hunter@me.com>
2014-01-30 09:22:29 +0000

    Final touches to old model, preparations for the horrors to come

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,8 +1,8 @@
 class Grid
 	attr_reader :cells
 
-	ROWS =    (0...9).map { |i| Array.new(9) { |i| i }.map { |v| v + (i*9) } }
-	COLUMNS = (0...9).map { |i| Array.new(9) { |i| i * 9 }.map { |v| v + i } }
+	ROWS =    (0..80).each_slice(9).to_a
+	COLUMNS = ROWS.transpose
 	SQUARES = [[0,1,2,9,10,11,18,19,20],[3,4,5,12,13,14,21,22,23],[6,7,8,15,16,17,24,25,26],[27,28,29,36,37,38,45,46,47],[30,31,32,39,40,41,48,49,50],[33,34,35,42,43,44,51,52,53],[54,55,56,63,64,65,72,73,74],[57,58,59,66,67,68,75,76,77],[60,61,62,69,70,71,78,79,80]]
 
 	def initialize(puzzle)
@@ -14,42 +14,54 @@ class Grid
 		@cells.all? { |cell| cell.solved? }
 	end
 
+
 	def index_of cell
 		@cells.index(cell)
 	end
 
 	def row_of cell
-		row = ROWS.select { |row| row.include?(index_of(cell)) }.flatten
-		row.map { |i| @cells[i].value }
+		values_in(surrounding(ROWS, cell))
 	end
 
 	def column_of cell
-		column = COLUMNS.select { |column| column.include?(index_of(cell)) }.flatten
-		column.map { |i| @cells[i].value }
+		values_in(surrounding(COLUMNS, cell))
 	end
 
-	def square_of cell
-		square = SQUARES.select { |square| square.include?(index_of(cell)) }.flatten
-		square.map { |i| @cells[i].value }
+	def square_of cell 
+		values_in(surrounding(SQUARES, cell))
+	end
+
+	def values_in area
+		area.flatten.map { |i| @cells[i].value }
+	end
+
+	def surrounding(areas, cell)
+		areas.select { |area| area.include?(index_of(cell)) }
 	end
 
 	def neighbours_of cell
 		values = row_of(cell) + column_of(cell) + square_of(cell) - [0]
-		values.uniq!.sort!
+		values.uniq.sort
 	end
+
 
 	def solve
 		outstanding_before, endless_looping = 81, false
 		while !solved? && !endless_looping
-			@cells.each do |c|
-				c.neighbours = neighbours_of(c)
-				c.solve
-			end
-			outstanding = @cells.count { |c| c.solved? }
+			try_to_solve_cells
+			outstanding = @cells.count { |cell| cell.solved? }
 			endless_looping = outstanding_before == outstanding
 			outstanding_before = outstanding
 		end
 	end
+
+	def try_to_solve_cells
+		@cells.each do |cell|
+			cell.neighbours = neighbours_of(cell)
+			cell.solve
+		end
+	end
+	
 
 	def inspect
 		@cells.map do |cell|
